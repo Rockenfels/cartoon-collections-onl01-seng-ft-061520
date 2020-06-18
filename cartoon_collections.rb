c74e549ec492e6ac6b346e6dd47474d8811fa2b4
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-18 19:03:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def roll_call_dwarves(roll)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  cap_calls = calls.map do |call|
+    call
+  end
 end
 
 def long_planeteer_calls# code an argument here
